7fa5bdaf3e1b88010cd686d497816941abac23a2
takeshi ogawa <takeshi050@gmail.com>
2019-06-15 21:34:02 +0900

    comments added to bikeshare.py and readme.md

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+#This is edited for git hub project
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
